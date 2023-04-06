eee05ab4b8f8d55a4663b3b893b6ef35792a0921
Dmitrii Svetlenko <dima.svetlenko@yandex.ru>
2023-04-06 20:30:26 +0300

    added new file in project-46

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+import { program } from ('commander');
+
+const program = new Command();
+
+program
+    .name('gendiff')
+    .usage('[options] <filepath1> <filepath2>')
+    .version('0.0.1')
+
+program.parse();
